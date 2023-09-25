41a87e16d0fb7b60bcea00a9bedb330df72c8a8e
Myles A. Knowlin <knowlim2@1038.org>
2023-09-25 19:44:49 -0400

    finished Exhibits added animallist to animal exhibits

--- exhibits/AnimalExhibit.java
+++ exhibits/AnimalExhibit.java
@@ -1,5 +1,12 @@
 package Zoo.exhibits;
 
-public abstract class AnimalExhibit {
-    
+import java.util.ArrayList;
+
+public abstract class AnimalExhibit extends Exhibit {
+
+    public ArrayList<Animal> animalList;
+    public AnimalExhibit(ExhibitType exhibitType, String name, ArrayList<Animal> animalList){
+        super(exhibitType, name);
+        this.animalList = animalList;
+    }
 }
